01c68288a3c949929189baa0c37409bcca9aff35
Joon Bak <jbdev98@gmail.com>
2025-12-17 22:38:16 +1100

    Complete minimal version

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/joonbak/lab
 
-go 1.22.2
+go 1.24.0
+
+toolchain go1.24.11
--- main.go
+++ main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
 func createDir(dir string) {
-	path := "./" + dir
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Failed to get gome directory:", err)
+		return
+	}
+
+	basePath := filepath.Join(home, "lab", "experiments")
+	if err := os.MkdirAll(basePath, 0755); err != nil {
+		fmt.Println("Failed to create base directory:", err)
+		return
+	}
+
+	path := filepath.Join(basePath, dir)
+	pathString := "~/lab/experiments/" + dir
 
 	if err := os.Mkdir(path, 0755); os.IsExist(err) {
 		fmt.Printf("Experiment '%s' already exists!\n", dir)
+		return
+	} else if err != nil {
+		fmt.Println("Failed to create directory:", err)
 	} else {
 		fmt.Printf("New experiment '%s' created!\n", dir)
 	}
+
+	fmt.Println("Copy below command to go to directory:")
+	fmt.Println("cd", pathString)
 }
 
 func main() {
@@ -22,10 +44,19 @@ func main() {
 
 	fmt.Println("Enter name of experiment:")
 	fmt.Printf("%s-", currentDate)
-	fmt.Scanln(&name)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		name = scanner.Text()
+	}
+
+	name = strings.ReplaceAll(name, " ", "-")
+
+	if name == "" {
+		fmt.Println("Name connot be empty!")
+		return
+	}
 
 	dir := currentDate + "-" + name
 	createDir(dir)
-	fmt.Println("Copy below command to go to directory")
-	fmt.Println("cd", dir)
 }
